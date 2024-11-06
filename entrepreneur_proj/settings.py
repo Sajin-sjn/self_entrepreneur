3856bd5af724f258e0b9614b98afaf8e03a0454d
Sajin PJ <sajinsjn4433@gmail.com>
2024-11-06 22:55:52 +0530

    setting confs , database mysql setting

--- entrepreneur_proj/settings.py
+++ entrepreneur_proj/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'entrepreneur_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'self_entrepreneur',
+        'HOST':'localhost',
+        'USER':'root',
+        'PASSWORD':'',
+        'PORT':3306
     }
 }
 
